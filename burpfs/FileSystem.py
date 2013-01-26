b87aea4ece8a2a1277f5373be6d72da493881bd4
Avi Rozen <avi.rozen@gmail.com>
2013-01-26 15:34:19 +0200

    removed delay between restores
    burp race condition seems to have been fixed by commit
    07fc4d853fdfffbbf1b7e0f9d4f35abedc49419d

--- burpfs/FileSystem.py
+++ burpfs/FileSystem.py
@@ -514,8 +514,6 @@ class FileSystem(Fuse):
             p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             stdout, stderr = p.communicate()
             self.logger.debug('%s%s' % (stdout, stderr))
-            # wait a bit before next restore
-            time.sleep(0.01)
         self._burp_set_status(FileSystem.burp_done)
         # unlock the configuration file
         self._burp_funlock(f)
